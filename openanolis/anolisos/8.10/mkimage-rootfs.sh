351ef37d8a346a547ba466cd2eba300a77dff0ec
Wenlong Zhang <zhangwenlong@loongson.cn>
2025-04-03 08:53:29 +0800

    update anolis 8.10-GA

--- openanolis/anolisos/8.10/mkimage-rootfs.sh
+++ openanolis/anolisos/8.10/mkimage-rootfs.sh
@@ -19,7 +19,7 @@ fi
 
 output="AnolisOS-${version}.rootfs.${arch}.tar.gz"
 
-repos_baseos_url="https://build.openanolis.cn/kojifiles/output/anolis-8-20250306.0/compose/BaseOS/loongarch64/os/"
+repos_baseos_url="https://build.openanolis.cn/kojifiles/output/anolis-8-20250401.0/compose/BaseOS/loongarch64/os/"
 
 #trap cleanup TERM EXIT
 
